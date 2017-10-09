d03502dfbd7bbb614eeb9e5da80678ffbf320530
fjoeg <workshopweekends@gmail.com>
2017-10-09 14:23:21 -0500

    Studio 6

---
+++ debounce/debounce.ino
@@ -0,0 +1,28 @@
+#define CHECK_INTERVAL 100
+
+const int buttonPin = 2;
+boolean buttonState;
+unsigned long checkButton;
+int pressCounts = 0;
+
+void buttonPressed() {
+  if(digitalRead(buttonPin) == LOW){
+    checkButton = millis() + CHECK_INTERVAL;
+  }
+  if(millis() >= checkButton){
+      pressCounts++;
+      Serial.print("Pressed: ");
+      Serial.print(pressCounts);
+      Serial.println(" times");
+  }
+}
+
+void setup() {
+  pinMode(buttonPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonPressed, CHANGE);
+  Serial.begin(9600);
+}
+
+void loop() {
+    
+}
--- src/studio6/SerialComm.java
+++ src/studio6/SerialComm.java
@@ -27,10 +27,55 @@ public class SerialComm {
 	}
 		
 	// TODO: Add writeByte() method from Studio 5
-	
+	public void writeByte(byte data) throws SerialPortException{
+	    port.writeByte(data);
+	    if(debug){
+	    	System.out.println("<0x" + Integer.toHexString(data) + ">");
+	    }
+	}
 	// TODO: Add available() method
-	
-	// TODO: Add readByte() method	
+	public boolean available(){
+		try {
+			if(port.getInputBufferBytesCount() > 0){
+				return true;
+			} else {
+				return false;
+			}
+		} catch (SerialPortException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false; 
+	}
+	// TODO: Add readByte() method		
+	public byte readByte(){
+		try {
+			byte bytes[] = port.readBytes(1);
+			if(debug){
+				System.out.println(String.format("%02x", bytes[0]));
+			}
+			return bytes[0];
+		} catch (SerialPortException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
 	
 	// TODO: Add a main() method
+	public static void main(String[] args){
+		SerialComm port = null;
+		try {
+			port = new SerialComm("COM5");
+		} catch (SerialPortException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//port.setDebug(true);
+		while(true){
+			if(port.available()){
+				System.out.println((char)port.readByte());
+			}
+		}
+	}
+	
 }
